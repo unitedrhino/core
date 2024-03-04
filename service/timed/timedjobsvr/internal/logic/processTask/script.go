a1ee0b07e2576f0d307079fa6821a406c5bc2f5b
godLei6 <603785348@qq.com>
2024-03-04 08:44:01 +0800

    feat: 对任务管理进行超级升级

---
+++ service/timed/timedjobsvr/internal/logic/processTask/script.go
@@ -0,0 +1,85 @@
+package processTask
+
+import (
+	"context"
+	"fmt"
+	"gitee.com/i-Things/core/service/timed/internal/domain"
+	"gitee.com/i-Things/core/service/timed/internal/repo/relationDB"
+	"gitee.com/i-Things/core/service/timed/timedjobsvr/internal/logic/processTask/sqlFunc"
+	"gitee.com/i-Things/share/errors"
+	"gitee.com/i-Things/share/utils"
+	"github.com/dop251/goja"
+	"github.com/spf13/cast"
+	"github.com/zeromicro/go-zero/core/logx"
+	"time"
+)
+
+func (t ProcessTask) ScriptExec(ctx context.Context, task *domain.TaskInfo) error {
+	var (
+		code = errors.OK.Code
+		msg  = errors.OK.GetMsg()
+	)
+	vm := goja.New()
+	sf := sqlFunc.NewSqlFunc(ctx, t.svcCtx, task, t.TaskSend, vm)
+
+	func() {
+		defer func() {
+			if p := recover(); p != nil {
+				if e, ok := p.(error); ok {
+					logx.WithContext(ctx).Errorf("panic err:%v", e)
+					err := errors.Fmt(e)
+					sf.ExecuteLog = append(sf.ExecuteLog, &domain.ScriptLog{
+						Level:       "error",
+						Content:     fmt.Sprintf("catch an panic,err:%v", err.Error()),
+						CreatedTime: time.Now().Unix(),
+					})
+					code = err.GetCode()
+					msg = err.GetMsg()
+				}
+			}
+		}()
+		var Run func(map[string]string) map[string]any
+		err := func() error {
+			err := sf.Register()
+			if err != nil {
+				return err
+			}
+			_, err = vm.RunString(task.Script.Param.ExecContent)
+			if err != nil {
+				return err
+			}
+			err = vm.ExportTo(vm.Get("Main"), &Run)
+			if err != nil {
+				return err
+			}
+			return nil
+		}()
+
+		e := errors.Fmt(err)
+		if e != nil {
+			logx.WithContext(ctx).Errorf("ScriptExec.err:%v", e)
+			code = e.GetCode()
+			msg = e.GetMsg()
+		} else if Run != nil {
+			ret := Run(task.Script.Param.Param)
+			code = cast.ToInt64(ret["code"])
+			msg = cast.ToString(ret["msg"])
+		}
+	}()
+	er := relationDB.NewJobLogRepo(ctx).Insert(ctx, &relationDB.TimedTaskLog{
+		GroupCode:          task.GroupCode,
+		TaskCode:           task.Code,
+		Params:             utils.MarshalNoErr(task.Script.Param.Param),
+		ResultCode:         code,
+		ResultMsg:          msg,
+		TimedTaskLogScript: &relationDB.TimedTaskLogScript{ExecLog: sf.ExecuteLog},
+		TimedTaskLogSql: &relationDB.TimedTaskLogSql{
+			SelectNum: sf.SelectNum,
+			ExecNum:   sf.ExecNum,
+		},
+	})
+	if er != nil {
+		logx.WithContext(ctx).Errorf("SqlExec.JobLog.Insert err:%v", er)
+	}
+	return er
+}
--- service/timed/timedjobsvr/internal/logic/processTask/sql.go
+++ service/timed/timedjobsvr/internal/logic/processTask/sql.go
@@ -2,17 +2,13 @@ package processTask
 
 import (
 	"context"
-	"fmt"
 	"gitee.com/i-Things/core/service/timed/internal/domain"
 	"gitee.com/i-Things/core/service/timed/internal/repo/relationDB"
-	"gitee.com/i-Things/core/service/timed/timedjobsvr/internal/logic/processTask/sqlFunc"
 	"gitee.com/i-Things/share/conf"
 	"gitee.com/i-Things/share/errors"
 	"gitee.com/i-Things/share/stores"
-	"github.com/dop251/goja"
 	"github.com/spf13/cast"
 	"github.com/zeromicro/go-zero/core/logx"
-	"time"
 )
 
 func (t ProcessTask) SqlExec(ctx context.Context, task *domain.TaskInfo) error {
@@ -43,7 +39,7 @@ func (t ProcessTask) SqlExec(ctx context.Context, task *domain.TaskInfo) error {
 	}()
 	e := errors.Fmt(err)
 	er := relationDB.NewJobLogRepo(ctx).Insert(ctx, &relationDB.TimedTaskLog{
-		Params:          task.Params,
+		Params:          task.Sql.Param.Sql,
 		ResultCode:      e.GetCode(),
 		ResultMsg:       e.GetMsg(),
 		TimedTaskLogSql: &relationDB.TimedTaskLogSql{ExecNum: execNum},
@@ -52,74 +48,4 @@ func (t ProcessTask) SqlExec(ctx context.Context, task *domain.TaskInfo) error {
 		logx.WithContext(ctx).Errorf("SqlExec.JobLog.Insert err:%v", er)
 	}
 	return err
-}
-
-func (t ProcessTask) ScriptExec(ctx context.Context, task *domain.TaskInfo) error {
-	var (
-		code = errors.OK.Code
-		msg  = errors.OK.GetMsg()
-	)
-	vm := goja.New()
-	sf := sqlFunc.NewSqlFunc(ctx, t.svcCtx, task, t.TaskSend, vm)
-
-	func() {
-		defer func() {
-			if p := recover(); p != nil {
-				if e, ok := p.(error); ok {
-					logx.WithContext(ctx).Errorf("panic err:%v", e)
-					err := errors.Fmt(e)
-					sf.ExecuteLog = append(sf.ExecuteLog, &domain.ScriptLog{
-						Level:       "error",
-						Content:     fmt.Sprintf("catch an panic,err:%v", err.Error()),
-						CreatedTime: time.Now().Unix(),
-					})
-					code = err.GetCode()
-					msg = err.GetMsg()
-				}
-			}
-		}()
-		var Run func(map[string]string) map[string]any
-		err := func() error {
-			err := sf.Register()
-			if err != nil {
-				return err
-			}
-			_, err = vm.RunString(task.Script.Param.ExecContent)
-			if err != nil {
-				return err
-			}
-			err = vm.ExportTo(vm.Get("Main"), &Run)
-			if err != nil {
-				return err
-			}
-			return nil
-		}()
-
-		e := errors.Fmt(err)
-		if e != nil {
-			logx.WithContext(ctx).Errorf("ScriptExec.err:%v", e)
-			code = e.GetCode()
-			msg = e.GetMsg()
-		} else if Run != nil {
-			ret := Run(task.Script.Param.Param)
-			code = cast.ToInt64(ret["code"])
-			msg = cast.ToString(ret["msg"])
-		}
-	}()
-	er := relationDB.NewJobLogRepo(ctx).Insert(ctx, &relationDB.TimedTaskLog{
-		GroupCode:          task.GroupCode,
-		TaskCode:           task.Code,
-		Params:             task.Params,
-		ResultCode:         code,
-		ResultMsg:          msg,
-		TimedTaskLogScript: &relationDB.TimedTaskLogScript{ExecLog: sf.ExecuteLog},
-		TimedTaskLogSql: &relationDB.TimedTaskLogSql{
-			SelectNum: sf.SelectNum,
-			ExecNum:   sf.ExecNum,
-		},
-	})
-	if er != nil {
-		logx.WithContext(ctx).Errorf("SqlExec.JobLog.Insert err:%v", er)
-	}
-	return er
 }
