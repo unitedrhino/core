c8387d4673561a957a9ab83fbc8406655dad3cbe
godLei6 <603785348@qq.com>
2024-05-14 10:13:07 +0800

    feat: 完善项目

--- service/timed/internal/repo/event/publish/pubJob/job.go
+++ service/timed/internal/repo/event/publish/pubJob/job.go
@@ -13,22 +13,31 @@ type (
 )
 
 func NewPubJob(c conf.EventConf) (*PubJob, error) {
-	natsJs, err := newNatsJsClient(c.Nats)
-	if err != nil {
-		return nil, err
+	switch c.Mode {
+	case conf.EventModeNats:
+		nats, err := newNatsClient(c.Nats)
+		if err != nil {
+			return nil, err
+		}
+		pj := PubJob{nats: nats}
+		return &pj, nil
+	case conf.EventModeNatsJs:
+		natsJs, err := newNatsJsClient(c.Nats)
+		if err != nil {
+			return nil, err
+		}
+		pj := PubJob{natsJs: natsJs}
+		return &pj, nil
 	}
-	nats, err := newNatsClient(c.Nats)
-	if err != nil {
-		return nil, err
-	}
-	pj := PubJob{natsJs: natsJs, nats: nats}
-	return &pj, nil
+	return nil, nil
+
 }
 func (p *PubJob) Publish(ctx context.Context, pubType string, topic string, payload []byte) error {
+	return p.nats.Publish(ctx, topic, payload)
 	if pubType == conf.EventModeNatsJs {
 		return p.natsJs.Publish(ctx, topic, payload)
 	} else {
-		return p.nats.Publish(ctx, topic, payload)
+
 	}
 	return nil
 }
