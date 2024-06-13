f8014f484d38eec07094d6c2e669c13d228ce9a2
godLei6 <603785348@qq.com>
2024-06-13 20:23:13 +0800

    feat: 完善项目

--- service/apisvr/internal/logic/system/tenant/info/updateLogic.go
+++ service/apisvr/internal/logic/system/tenant/info/updateLogic.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"gitee.com/i-Things/core/service/apisvr/internal/logic/system"
 	"gitee.com/i-Things/share/ctxs"
+	"gitee.com/i-Things/share/utils"
 
 	"gitee.com/i-Things/core/service/apisvr/internal/svc"
 	"gitee.com/i-Things/core/service/apisvr/internal/types"
@@ -26,6 +27,7 @@ func NewUpdateLogic(ctx context.Context, svcCtx *svc.ServiceContext) *UpdateLogi
 }
 
 func (l *UpdateLogic) Update(req *types.TenantInfo) error {
+	defer utils.Recover(l.ctx)
 	if err := ctxs.IsRoot(l.ctx); err != nil {
 		return err
 	}
