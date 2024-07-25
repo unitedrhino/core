117782fb14576f52b8d761b28900f8f903ade854
godLei6 <603785348@qq.com>
2024-07-25 17:22:37 +0800

    feat: 完善项目

---
+++ service/apisvr/internal/logic/system/tenant/app/updateLogic.go
@@ -0,0 +1,31 @@
+package app
+
+import (
+	"context"
+	"gitee.com/i-Things/core/service/syssvr/pb/sys"
+	"gitee.com/i-Things/share/utils"
+
+	"gitee.com/i-Things/core/service/apisvr/internal/svc"
+	"gitee.com/i-Things/core/service/apisvr/internal/types"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type UpdateLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+func NewUpdateLogic(ctx context.Context, svcCtx *svc.ServiceContext) *UpdateLogic {
+	return &UpdateLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *UpdateLogic) Update(req *types.TenantAppInfo) error {
+	_, err := l.svcCtx.TenantRpc.TenantAppUpdate(l.ctx, utils.Copy[sys.TenantAppInfo](req))
+	return err
+}
