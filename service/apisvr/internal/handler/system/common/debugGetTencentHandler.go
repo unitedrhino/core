b337f3a6834a0d7cd676b49c4a6f9e266466eec5
godLei6 <603785348@qq.com>
2024-08-14 17:29:27 +0800

    feat: 完善项目

--- service/apisvr/internal/handler/system/common/debugGetTencentHandler.go
+++ service/apisvr/internal/handler/system/common/debugGetTencentHandler.go
@@ -3,19 +3,14 @@ package common
 import (
 	"net/http"
 
-	"gitee.com/i-Things/share/result"
-
-	"gitee.com/i-Things/core/service/apisvr/internal/logic/system/common"
 	"gitee.com/i-Things/core/service/apisvr/internal/svc"
 )
 
 func DebugGetTencentHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		l := common.NewDebugGetTencentLogic(r.Context(), svcCtx)
-		err := l.DebugGetTencent()
 		w.WriteHeader(http.StatusOK)
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.Write([]byte(r.Header.Get("echostr")))
-		result.Http(w, r, nil, err)
+		//result.Http(w, r, nil, err)
 	}
 }
