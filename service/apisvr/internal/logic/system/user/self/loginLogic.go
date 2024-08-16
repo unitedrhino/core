b36e79f0dd5e5b8ff7fa373cf28e22ec4ab8f9ea
godLei6 <603785348@qq.com>
2024-08-16 10:18:36 +0800

    feat: 完善项目

--- service/apisvr/internal/logic/system/user/self/loginLogic.go
+++ service/apisvr/internal/logic/system/user/self/loginLogic.go
@@ -92,17 +92,23 @@ func (l *LoginLogic) Login(req *types.UserLoginReq) (resp *types.UserLoginResp, 
 		return nil, er
 	}
 	//登录成功记录
-	l.svcCtx.LogRpc.LoginLogCreate(l.ctx, &sys.LoginLogCreateReq{
-		AppCode:       uc.AppCode,
-		UserID:        uResp.Info.UserID,
-		UserName:      uResp.Info.UserName,
-		IpAddr:        ctxs.GetUserCtx(l.ctx).IP,
-		LoginLocation: GetCityByIp(ctxs.GetUserCtx(l.ctx).IP),
-		Browser:       browser,
-		Os:            os,
-		Msg:           "登录成功",
-		Code:          200,
+	ctxs.GoNewCtx(l.ctx, func(ctx context.Context) {
+		_, err := l.svcCtx.LogRpc.LoginLogCreate(ctx, &sys.LoginLogCreateReq{
+			AppCode:       uc.AppCode,
+			UserID:        uResp.Info.UserID,
+			UserName:      uResp.Info.UserName,
+			IpAddr:        ctxs.GetUserCtx(ctx).IP,
+			LoginLocation: GetCityByIp(ctxs.GetUserCtx(ctx).IP),
+			Browser:       browser,
+			Os:            os,
+			Msg:           "登录成功",
+			Code:          200,
+		})
+		if err != nil {
+			logx.WithContext(ctx).Error(err)
+		}
 	})
+
 	info, err := l.svcCtx.UserRpc.UserRoleIndex(l.ctx, &sys.UserRoleIndexReq{
 		UserID: uResp.Info.UserID,
 	})
