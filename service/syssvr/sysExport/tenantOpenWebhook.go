774a556e96efe33729e67f1e047c28c3f3ce85ea
godLei6 <603785348@qq.com>
2024-12-09 17:33:32 +0800

    feat: webhook添加设备事件通知

--- service/syssvr/sysExport/tenantOpenWebhook.go
+++ service/syssvr/sysExport/tenantOpenWebhook.go
@@ -16,6 +16,7 @@ import (
 const (
 	CodeDmDeviceConn           = "dmDeviceConn"
 	CodeDmDevicePropertyReport = "devicePropertyReport"
+	CodeDmDeviceEventReport    = "deviceEventReport"
 )
 
 type Webhook struct {
