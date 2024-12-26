3e228d123a054a3f02b4afdf5975d160cb0beab3
godLei6 <603785348@qq.com>
2024-12-26 22:47:31 +0800

    feat: 功能增强

--- shell/gitpush.sh
+++ shell/gitpush.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 set -x
-# 检查是否至少有一个参数
-if [ $# -eq 0 ]; then
-    echo "请输入tag标签。"
-    exit 1
-fi
 
-
-# 获取第一个参数
-tag="$1"
-git tag $tag
 git push -f  gitee dev:master
 git push -f  github dev:master
-git push origin $tag
-git push gitee $tag
-git push github $tag
