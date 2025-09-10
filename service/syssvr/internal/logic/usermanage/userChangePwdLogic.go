21be9e4ad744eaf04b68ab217ac2a9a768190dea
godLei6 <603785348@qq.com>
2025-09-10 14:54:35 +0800

    feat: 修改密码增强

--- service/syssvr/internal/logic/usermanage/userChangePwdLogic.go
+++ service/syssvr/internal/logic/usermanage/userChangePwdLogic.go
@@ -2,6 +2,7 @@ package usermanagelogic
 
 import (
 	"context"
+
 	"gitee.com/unitedrhino/core/service/syssvr/internal/repo/relationDB"
 	"gitee.com/unitedrhino/core/share/users"
 	"gitee.com/unitedrhino/share/ctxs"
@@ -39,9 +40,12 @@ func (l *UserChangePwdLogic) UserChangePwd(in *sys.UserChangePwdReq) (*sys.Empty
 		if account == "" {
 			return nil, errors.Captcha
 		}
-		ui, err := relationDB.NewUserInfoRepo(l.ctx).FindOneByFilter(l.ctx, relationDB.UserInfoFilter{Accounts: []string{account}})
+		ui, err := relationDB.NewUserInfoRepo(l.ctx).FindOne(l.ctx, uc.UserID)
 		if err != nil {
 			return nil, err
+		}
+		if ui.Email.String != account {
+			return nil, errors.UnBindAccount
 		}
 		oldUi = ui
 	case def.CaptchaTypePhone:
@@ -49,24 +53,33 @@ func (l *UserChangePwdLogic) UserChangePwd(in *sys.UserChangePwdReq) (*sys.Empty
 		if account == "" {
 			return nil, errors.Captcha
 		}
-		ui, err := relationDB.NewUserInfoRepo(l.ctx).FindOneByFilter(l.ctx, relationDB.UserInfoFilter{Accounts: []string{account}})
+		ui, err := relationDB.NewUserInfoRepo(l.ctx).FindOne(l.ctx, uc.UserID)
 		if err != nil {
 			return nil, err
+		}
+		if ui.Phone.String != account {
+			return nil, errors.UnBindAccount
 		}
 		oldUi = ui
 	case users.RegPwd:
-		account = l.svcCtx.Captcha.Verify(l.ctx, def.CaptchaTypeImage, def.CaptchaUseChangePwd, in.CodeID, in.Code)
-		if account == "" {
-			return nil, errors.Captcha
+		if in.Code != "" {
+			if l.svcCtx.Captcha.Verify(l.ctx, def.CaptchaTypeImage, def.CaptchaUseChangePwd, in.CodeID, in.Code) == "" {
+				return nil, errors.Captcha
+			}
+		} else if l.svcCtx.LoginLimit.PwdCaptcha.CheckLimit(l.ctx, "changePwd:"+uc.Account) {
+			return nil, errors.NeedImgCaptcha
 		}
-		ui, err := relationDB.NewUserInfoRepo(l.ctx).FindOneByFilter(l.ctx, relationDB.UserInfoFilter{Accounts: []string{account}})
+		l.svcCtx.LoginLimit.PwdCaptcha.LimitIt(l.ctx, "changePwd:"+uc.Account)
+		ui, err := relationDB.NewUserInfoRepo(l.ctx).FindOne(l.ctx, uc.UserID)
 		if err != nil {
 			return nil, err
 		}
-		//md5加密后的密码则通过二次md5加密再对比库中的密码
-		password1 := utils.MakePwd(in.OldPassword, uc.UserID, true) //对密码进行md5加密
-		if password1 != ui.Password {
-			return nil, errors.Password
+		if ui.Password != "" {
+			//md5加密后的密码则通过二次md5加密再对比库中的密码
+			password1 := utils.MakePwd(in.OldPassword, uc.UserID, true) //对密码进行md5加密
+			if password1 != ui.Password {
+				return nil, errors.Password
+			}
 		}
 		oldUi = ui
 	}
